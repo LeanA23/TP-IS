0e039584fd99742507791c42dd6146a07e0507a9
DESKTOP-AL5H48L\leandro <arielchanampa12@gmail.com>
2021-06-30 23:03:17 -0300

    prueba

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 var calcu = require('./aplicacion/calculadora');
 
 var a=8;
-var b=2;
+var b=3;
 
 console.log(`Suma ${a}+${b} = ${calcu.suma(a,b)}`);
 console.log(`Resta ${a}+${b} = ${calcu.resta(a,b)}`);
